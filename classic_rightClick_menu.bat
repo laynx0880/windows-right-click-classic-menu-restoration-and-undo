1159fb29bdd0282de1131f310f318b6e42938d75
laynx0880 <109639650+laynx0880@users.noreply.github.com>
2026-07-31 09:42:37 -0500

    Add files via upload

---
+++ classic_rightClick_menu.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Add the registry key to enable the classic Windows 11 context menu
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+:: Restart Windows Explorer to apply changes instantly
+taskkill /f /im explorer.exe
+start explorer.exe
+
+echo Context menu updated!
+pause
---
+++ undo_classic_rightClick_menu.bat
@@ -0,0 +1,12 @@
+
+
+@echo off
+:: Delete the registry key to restore the default Windows 11 context menu
+reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
+
+:: Restart Windows Explorer to apply changes instantly
+taskkill /f /im explorer.exe
+start explorer.exe
+
+echo Modern context menu restored!
+pause

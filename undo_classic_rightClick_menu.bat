54e9c2dd30769502c70da8ddb58a62047c6b7cf4
laynx0880 <109639650+laynx0880@users.noreply.github.com>
2026-07-31 09:50:36 -0500

    Update undo_classic_rightClick_menu.bat

--- undo_classic_rightClick_menu.bat
+++ undo_classic_rightClick_menu.bat
@@ -1,10 +1,10 @@
 
 
 @echo off
-:: Delete the registry key to restore the default Windows 11 context menu
+rem Delete the registry key to restore the default Windows 11 context menu
 reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
 
-:: Restart Windows Explorer to apply changes instantly
+rem Restart Windows Explorer to apply changes instantly
 taskkill /f /im explorer.exe
 start explorer.exe
 
